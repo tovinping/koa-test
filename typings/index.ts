587c5393edd1eb92b14a685b641f8c3790329e61
DESKTOP-H350DI6\tovinping <244490027@qq.com>
2021-09-05 16:11:27 +0800

    typings

--- typings/index.ts
+++ typings/index.ts
@@ -1,4 +1,9 @@
 import { Context } from 'koa'
+export * from './conversation'
+export * from './message'
+export * from './user'
+export * from './group'
+export * from './member'
 declare global {
   interface KoaCtx extends Context {
     success(data?: any, msg?: string, code?: number):void
