1e063d60cf8ccb7944110bd6d89f5a6a762b5ae3
tovinping <tovinping@qq.com>
2021-12-29 20:48:49 +0800

    天所冷了

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,13 @@
 export * from './response'
 export * from './encrypt'
 export * from './token'
+
+export function isEmpty(val?: any) {
+  return !(val
+    ? typeof val === 'object'
+      ? Array.isArray(val)
+        ? !!val.length
+        : !!Object.keys(val).length
+      : true
+    : false)
+}
