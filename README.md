0df57e85e33540048269b7e877312174eecc9c7b
tovinping <tovinping@qq.com>
2022-04-06 00:12:15 +0800

    history

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# koa-test
+# service
 
 [cos](https://github.com/tencentyun/cos-js-sdk-v5/blob/master/server/sts.js)
+
+# DB 设计
+
+## 最近会话
+
+| 字段名称  | 取值    | 说明                        |
+| --------- | ------- | --------------------------- |
+| account   | string  | 创建者账号                  |
+| type      | string  | 1 群聊 2 单聊               |
+| chatId    | 会话 ID | 单聊天为对方账号群聊为群 ID |
+| name      | string  | 单聊天为姓名群聊为群名称    |
+| lastMsgId | string  | 最后一条消息 ID             |
+| isTop     | number  | 1 置顶 0 正常               |
+
+## 消息
+
+| 字段名称  | 取值   | 说明                        |
+| --------- | ------ | --------------------------- |
+| msgId     | string | 消息 ID                     |
+| account   | string | 发送者帐号                  |
+| name      | string | 发送者名称                  |
+| chatType  | string | 最后一条消息 ID             |
+| chatId    | string | 单聊为对方 ID 群聊天为群 ID |
+| content   | string | 消息内容                    |
+| type      | string | 消息类型                    |
+| timestamp | number | 发送时间                    |
--- service/msg.ts
+++ service/msg.ts
@@ -2,24 +2,32 @@ import Router from 'koa-router'
 import { getLogger } from '../utils/logger'
 import { Msg } from '../models'
 import { responseError, responseSuccess } from '../utils'
+import { CHAT_TYPE } from '../constant'
 const router = new Router()
 const logger = getLogger('service_msg')
 
 router.get('/history', async ctx => {
-  const { chatId, timestamp = 0 } = ctx.query
+  const { chatId, chatType, timestamp = 0 } = ctx.query
   const { account } = ctx.state.token
   logger.info('history chatId=', chatId, 'account=', account, 'timestamp=', timestamp)
-  if (!chatId) {
+  if (!chatId || !chatType) {
     ctx.body = responseError({ msg: '参数错误' })
     return
   }
-  const findResult = await Msg.find({ account, chatId, timestamp: { $lt: Number(timestamp) } })
-    .limit(20)
-    .sort({ timestamp: -1 })
-  if (findResult) {
+  if (chatType === CHAT_TYPE.P2P) {
+    const findResult = await Msg.find({ timestamp: { $lt: Number(timestamp) } })
+      .or([
+        { chatId: account, account: chatId },
+        { account, chatId },
+      ])
+      .limit(20)
+      .sort({ timestamp: -1 })
     ctx.body = responseSuccess({ body: findResult })
   } else {
-    ctx.body = responseError({ msg: '查询出错' })
+    const findResult = await Msg.find({ chatId, timestamp: { $lt: Number(timestamp) } })
+      .limit(20)
+      .sort({ timestamp: -1 })
+    ctx.body = responseSuccess({ body: findResult })
   }
 })
 
